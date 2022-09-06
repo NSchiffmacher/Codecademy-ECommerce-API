c31ff59c11ddd6b776a22022c2e969005a0a6546
ElBrazilian <nathan.schiffmacher@gmail.com>
2022-09-06 18:36:34 +0200

    Setting up basic middlewares: body-parser, cors, morgan

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+
+// ENV Variables
+const PORT = process.env.PORT || 3000;
+
+// Server creation
+const app = express();
+
+// Middleware setup
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+// Base routes
+app.get('/', (req, res, next) => {
+    res.send('Hello World');
+})
+
+// Server start
+app.listen(PORT, () => {
+    console.log(`API listening on port ${PORT}...`);
+})
